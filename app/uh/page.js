84cbdb4669ca0402a1a1ca3a7be51ecb4e1bd1a5
samak519 <samak519@student.liu.se>
2023-06-02 12:36:37 +0200

    trying to fix stuf.

--- app/uh/page.js
+++ app/uh/page.js
@@ -1,9 +1,14 @@
 import styles from 'app/page.module.css'
 
-export default function Uh() {
+export default function Uh () {
   return (
     <div className={styles.main}>
       <h1>Uhhh.</h1>
     </div>
   )
+}
+
+export const metadata = {
+  title : 'Uh',
+  description : 'Uh',
 }
