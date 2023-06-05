815a108a186fd2034a35a603f668a2ad30e4f478
samak519 <samak519@student.liu.se>
2023-06-05 11:44:51 +0200

    addded base blog

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+var DOMAIN = 'samake.se'
+module.exports = {
+	title: 'Eleventy Base Blog v8',
+	url: 'https://' + DOMAIN + '/',
+	language: 'sv',
+	description: 'Min hemsida, byggd med Eleventy',
+	author: {
+		name: 'Samuel Åkesson',
+		email: 'samak519@student.liu.se',
+	}
+}
